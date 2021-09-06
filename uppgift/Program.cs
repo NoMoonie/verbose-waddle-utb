75e3b7df2f0d057e37ba06354e0819d5aeb5083a
NoMoonie <46845957+NoMoonie@users.noreply.github.com>
2021-09-06 18:02:29 +0200

    init commit

---
+++ uppgift/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using uppgiftlibrary;
+
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ItemModel itemModel = new ItemModel
+            {
+                ItemName = "test",
+                ItemID = "23"
+            };
+
+            System.Console.WriteLine($"Name: {itemModel.ItemName} ID: {itemModel.ItemID}");
+
+        }
+    }
+}
---
+++ uppgiftlibrary/ItemModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace uppgiftlibrary{
+    public class ItemModel{
+
+        public string ItemName { get; set; }
+        public string ItemID { get; set; }
+    }
+}
